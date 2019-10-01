75445946a679b14b60b3eb1b06a8d44cb78cf27b
Ella <michaella.latkovic@gmail.com>
2019-09-30 18:59:55 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.querySelector('form').addEventListener('submit', onSubmit)
 });
+
+function onSubmit(e){
+  e.preventDefault()
+
+  const newTask = document.querySelector('input').value
+  let newLi = document.createElement('li')
+  newLi.innerHTML = newTask
+
+  document.querySelector('#tasks').appendChild(newLi)
+  document.querySelector('input').value = ""
+}
