3f3b82eb9918b0e1a1a7ba397543b2048941b632
tan-nate <nktan93@gmail.com>
2019-10-01 09:37:40 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  let submit_button = document.querySelector("input[type=submit]")
+  submit_button.addEventListener("click", function(event) {
+    event.preventDefault()
+    let li = document.createElement('li')
+    let input = document.querySelector("input#new-task-description")
+    li.innerHTML = `${input.value}<button>x</button>`
+    document.querySelector("ul#tasks").appendChild(li)
+    input.value = ""
+    let x_buttons = document.querySelectorAll("button")
+    x_buttons.forEach(function(element) {
+      element.addEventListener("click", function() {
+        element.parentElement.remove()
+      })
+    })
+  })
+})
