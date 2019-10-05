f3fb3a10cd5cc0a7456f82b577b2fd85ea622c97
Jennifer Pazos <jmp521@gmail.com>
2019-10-04 20:22:22 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+
+document.addEventListener("DOMContentLoaded", function(e) {
+  
+  const form = document.getElementById("create-task-form")
+  
+  let submitText = document.getElementById("new-task-description")
+  let getUL = document.getElementById("tasks")
+  let buttons = document.querySelectorAll("button")
+  buttons.forEach(function(b) {
+    b.addEventListener("click", function() {
+      deleteMe()
+    })
+  })  
+  
+  form.addEventListener("submit", function() {
+    event.preventDefault()
+    addLiToDom()
+    
+    
+  })
+  
+  function addLiToDom() {
+    let createLI = document.createElement("li")
+    
+    createLI.innerHTML = submitText.value
+   
+
+    getUL.appendChild(createLI)
+    submitText.value = ""
+    
+  }
+
+
+})
+
+
