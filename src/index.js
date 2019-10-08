63e008f1072820688234f4a0846cd62ecd03df9d
Tsunamins <reillyamr@gmail.com>
2019-10-08 19:12:17 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
   const newTaskForm = document.getElementById("create-task-form");
-    const newTaskDescription = document.getElementById("new-task-description");
-    const newTaskPriority = document.getElementById("new-task-priority");
-
-    // grab the ul where new tasks will live on the DOM
-    const newTaskUl = document.getElementById("tasks");
-
     // attach event listener on submit
     newTaskForm.addEventListener("submit", createNewTask);
 
