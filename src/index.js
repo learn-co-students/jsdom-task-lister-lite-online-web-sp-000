d8fc175428edb966dd836d44c7753aa9094f59ee
Kyle  Beckstead <kyle21beckstead@gmail.com>
2019-10-10 19:46:35 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+document.addEventListener("DOMContentLoaded", handleEverything);
+
+function handleEverything(event) {
+	let form = document.querySelector("#create-task-form");
+	form.addEventListener("submit", handleFormSubmit);
+}
+
+function handleFormSubmit(event) {
+	event.preventDefault();
+	let todoitem = event.target["new-task-description"].value;
+
+	let tasks = document.querySelector("#tasks");
+	let li = document.createElement("li");
+	let button = document.createElement("button");
+	button.innerText = "Delete";
+	button.id = "delete";
+	li.innerText = todoitem;
+	tasks.appendChild(li);
+	li.appendChild(button);
+
+	button.addEventListener("click", handleDelete);
+}
+
+function handleDelete(event) {
+	let parent = event.target.parentNode;
+	parent.parentNode.removeChild(parent)
+};
