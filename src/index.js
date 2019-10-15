a0f44cada9033279d6fd134cd4374af624987642
davisjustinw <davisjustinw@gmail.com>
2019-10-14 21:10:08 -0700

    deliverables met, added delete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let submit = document.querySelector("#create-task-form input[type='submit']")
+
+  submit.addEventListener("click", event => {
+    event.preventDefault()
+
+    let newTask = document.getElementById( 'new-task-description' ).value
+    let taskList = document.getElementById( 'tasks' )
+    let newElement = document.createElement( 'li' )
+    let newButton = document.createElement( 'button' )
+
+    newElement.textContent = newTask
+    newButton.setAttribute( 'data-description', newTask )
+    newButton.textContent = 'X'
+    newButton.addEventListener("click", event => {
+      event.currentTarget.parentNode.remove()
+    })
+
+    newElement.appendChild( newButton )
+    taskList.appendChild( newElement )
+
+  })
+
 });
