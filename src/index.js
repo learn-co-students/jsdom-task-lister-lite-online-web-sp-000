7a33cbae61650b1656dc6a51f001ec9651dcf2ce
Jane Venter <janeanneventer@gmail.com>
2019-10-15 19:37:26 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.querySelector('[type=submit]').addEventListener("click", function(event){
+    event.preventDefault();
+  });
+    document.querySelectorAll('[type=submit]').addEventListenter("click", function(event){
+      let input = document.getElementById("new-task-description").value;
+
+      let tasks = document.getElementById("tasks");
+      let li = tasks.appendChild("li");
+      li.innerText = input;
+  });
+
 });
