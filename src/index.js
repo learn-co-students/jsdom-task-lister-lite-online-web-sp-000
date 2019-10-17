7b5948b751e800f377b574cba1debd4b7ccc1926
Daniel Dawson <dannyldawson@gmail.com>
2019-10-16 17:54:02 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById("create-task-form");
+  const todoList = document.getElementById("tasks");
+
+  form.addEventListener("submit", e => {
+    e.preventDefault();
+    console.log("event fired!");
+    const input = document.getElementById("new-task-description").value;
+    const li = document.createElement("li");
+    li.classList.add("list-item");
+    li.addEventListener("click", e => removeElement(e));
+    li.textContent = input;
+    todoList.appendChild(li);
+  });
+
+  const removeElement = e => {
+    e.target.remove();
+  };
 });
