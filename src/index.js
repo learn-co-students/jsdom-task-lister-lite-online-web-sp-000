d0d76ba82db31295409d1d1431b406037acaa199
Adam Robinson <luohaoxiang@yahoo.com>
2019-10-26 19:07:08 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -4,33 +4,37 @@ document.addEventListener("DOMContentLoaded", () => {
 const textForm = document.querySelector("#create-task-form")
 const textInput = document.getElementById("new-task-description")
 const unorderedList = document.querySelector("#tasks")
+const listDiv = document.querySelector("#list")
+
 
   textForm.addEventListener('submit', () => {
     event.preventDefault()
     console.log(textInput.value)
     const taskLi = document.createElement('li')
     const deleteButton = document.createElement('button')
+    deleteButton.className = 'delete'
     deleteButton.innerText = "Delete"
     const formValue = textInput.value
    taskLi.innerText = formValue
     unorderedList.appendChild(taskLi)
     taskLi.appendChild(deleteButton)
-    deleteButton.addEventListener('click', (event) => {
-      if (event) {
-        taskLi.remove()
+    
+    // if (event.target === )
+
+    textForm.reset() 
+
+    unorderedList.addEventListener('click', (event) => {
+      if (event.target.className === 'delete') {
+        event.target.parentNode.remove()
       }
     })
 
-    textForm.reset() 
-  
-   
-    
-    
-
-  })
+  }) //close of textForm event listener
 
   
 
   
 
-});
+  
+
+}); // close of window event listener
