5d77a00743993d8c9dd514376d53f3f6d646fe1a
chadeyrasal <annelaure.chadeyras@gmail.com>
2019-11-12 14:05:59 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,20 @@
-document.addEventListener("DOMContentLoaded", (event) => {
-  event.preventDefault();
-  
+document.addEventListener("DOMContentLoaded", () => {
+
+  createListItem()
+
 });
+
+
+function createListItem() {
+
+  document.querySelector('form').addEventListener('submit', function(event){
+    event.preventDefault()
+
+    let item = document.querySelector('input').value
+    let li = document.createElement('li')
+    li.innerHTML = item + '<button>x</button>'
+    document.getElementById('tasks').appendChild(li)
+
+    document.querySelector('input').value = ""
+  })
+}
