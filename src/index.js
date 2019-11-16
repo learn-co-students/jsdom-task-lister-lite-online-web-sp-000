38b48ecbaefba8f7f164546e5ce4ff05040f1c2a
jon <jonathan.wong110@gmail.com>
2019-11-16 14:54:29 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const taskForm = document.getElementById("create-task-form")
+  const taskItemUl = document.getElementById("tasks")
+  taskForm.addEventListener("submit", function(event) {
+    event.preventDefault();
+    const taskInput = document.getElementById("new-task-description")
+    const newTask = document.createElement("li");
+    newTask.innerText = taskInput.value
+    taskItemUl.appendChild(newTask)
+    debugger;
+  })  
+  
 });
