a6bbe697770cd9a2c67e108dc8a97c8f95f25a31
Kristina Mari Horensky <kristinahorensky@gmail.com>
2019-11-24 11:43:37 -0800

    Done.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-  ("#create-task-form").submit(function(e) {
+  document.querySelector("#create-task-form").addEventListener("submit", function (e) {
     let element = document.querySelector("ul#tasks");
     element.innerHTML = "new-task-description";
     e.preventDefault();
