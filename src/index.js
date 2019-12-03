ff17ae4dc54aa5c1bf1a74321deea58d0319582c
root <root@frobot.localdomain>
2019-12-03 14:12:00 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const list = new List();
+  const newTaskForm = document.getElementById('create-task-form');
+  const newTaskDescription = document.getElementById('new-task-description');
+  const tasksTag = document.getElementById('tasks')
+  const magic = () => (tasksTag.innerHTML = list.showTasks());
+  newTaskForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    list.createNewTask(newTaskDescription.value);
+    e.target.reset();
+    magic();
+  })
+  tasksTag.addEventListener('click', (e) => {
+    if (e.target.nodeName==='BUTTON'){
+      list.destroyTask(e.target.dataset.description);
+      magic();
+    }
+  })
 });
