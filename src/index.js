f3adab10e3cbdb0aa2ea54c939e7cf9ab5447879
Yingqi Chen <chanwk18@hotmail.com>
2019-12-15 21:11:14 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
   const form = document.getElementById('create-task-form');
-  let taskDescription = document.getElementById('new-task-description').value
+  let taskDescription = document.getElementById('new-task-description')
+
   function logSubmit(event) {
     event.preventDefault();
-    document.getElementById("list").innerHTML += `<li> ${taskDescription} </li>`;
+    document.getElementById("list").innerHTML += `<li> ${taskDescription.value} <button id=${taskDescription.value} onclick= this.parentElement.remove()>X</button> </li>`;
+    taskDescription.value = " "
+
   }
+
 
   form.addEventListener('submit', logSubmit);
 });
