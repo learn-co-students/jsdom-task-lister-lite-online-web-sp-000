d7c1955a8b16bdc459577e96a5cd8e98bfd3929c
boom100100 <bsd231@nyu.edu>
2019-12-26 14:27:46 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+
+  const form = document.getElementById('create-task-form');
+
+  form.onsubmit = function(){
+
+    //log.textContent = `Form Submitted! Time stamp: ${event.timeStamp}`;
+    //preventDefault();
+
+    //select list, create list item
+    let uList = document.querySelector('ul');
+    let lItem = document.createElement('li');
+
+    //select text from form, add to list item text container
+    let formContent = document.getElementById("new-task-description");
+    let lItemText = document.createTextNode(formContent.value);
+
+    //add list item text container to list item
+    //add list item to list container
+    lItem.appendChild(lItemText);
+    uList.appendChild(lItem);
+
+    return false; //so it doesn't post to action
+  }
 });
