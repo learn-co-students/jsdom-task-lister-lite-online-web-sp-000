c952ab05cf22dd16927874d3cea4be2aa9162170
Karen V <karenvo@usc.edu>
2019-12-28 05:28:55 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  sub = document.getElementById("create-task-form")
+  sub.addEventListener("submit", function(event){
+    event.preventDefault()
+  })
 });
