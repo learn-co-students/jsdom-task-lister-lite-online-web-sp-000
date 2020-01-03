8c46d7d8f76ac3607832fc8dddd199141a135b25
stabassum <sumu1993@gmail.com>
2020-01-03 10:24:10 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
 });
+
+
+document.getElementById("submit").addEventListener('click', function(event){
+  // debugger;
+  let grabInput = document.getElementById("new-task-description").value;
+
+
+    document.getElementById('tasks').innerHTML = grabInput;
+  
+    
+  event.preventDefault();
+});
