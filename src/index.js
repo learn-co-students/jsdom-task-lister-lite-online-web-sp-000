41a1efc5c226786ec20139a9ed18d0bfcd516da7
github username <github email address>
2020-01-08 22:08:49 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById("create-task-form");
+  const ul = document.getElementById("tasks");
+
+  form.addEventListener("submit", event => {
+    event.preventDefault();
+
+    const input = document.getElementById("new-task-description").value;
+    const li = document.createElement("li");
+    const taskPriority = document.getElementById("taskPriority");
+    const dueDate = document.getElementById("due-date").value;
+
+    li.innerHTML = `${input} ${dueDate} <button>X</button>`;
+
+    ul.appendChild(li);
+
+    li.className = taskPriority.value
+
+    li.querySelector("button").addEventListener("click", event => {
+      li.parentNode.removeChild(li);
+    });
+  });
 });
