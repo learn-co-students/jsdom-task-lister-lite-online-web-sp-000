5a31e8e4a278731d06ca558784377601f21af2a5
Lindsey Wells <github email address>
2020-01-09 13:34:30 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+document.getElementById("submit").addEventListener("click", function(event) {
+    event.preventDefault();
+    let task = document.getElementById("new-task-description").value;
+    let element = document.createElement("li")
+    element.innerHTML = `${task} <button data-description="${task}">X</button>`
+    let list = document.getElementById("tasks")
+    list.appendChild(element)
+
+})
+
+let buttons = document.getElementsByTagName("button")
+
+buttons
