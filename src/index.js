8f1c41e9a140472c699948929c69314c44c8ccfa
Lauren McCoy <lm_lutec@outlook.com>
2020-01-30 10:16:33 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+   let e = document.getElementById('create-task-form').addEventListener("submit", function(event){
+   event.preventDefault()
+
+    let text = document.getElementById('new-task-description').value 
+    let test = document.createElement("LI")
+    let button = document.createElement("BUTTON")
+    button.innerText = "X"
+    let g = document.getElementById('tasks').appendChild(test)
+    g.append(text)
+    g.append(button)
+
+    let del = document.getElementById('tasks').addEventListener("click", function(event) {
+      event.target.parentElement.remove()
+    
+    })
+  });
 });
