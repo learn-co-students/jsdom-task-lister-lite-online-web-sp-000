910adeb842fb0035e428db3bfa134fbf159dfc26
tomciv9000 <eugene.white@gmail.com>
2020-02-03 17:33:41 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
-  
   const newTaskForm = document.getElementById("create-task-form");
   const newTaskDescription = document.getElementById("new-task-description");
   const newTaskPriority = document.getElementById("new-task-priority");
-
-  //ul where new tasks will live on the DOM
-  //const newTaskUl = document.getElementById("tasks");
-
-  //attach event listeners
+  const newTaskUl = document.getElementById("tasks");
   newTaskForm.addEventListener("submit", createNewTask);
 });
 
