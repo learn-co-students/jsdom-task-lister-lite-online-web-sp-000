d7b15d093bd987cc5c1d8749ee092e5327c915c9
github username <github email address>
2020-02-06 14:17:46 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  let tasks = document.getElementById('tasks');
+  
+
+  document.addEventListener('submit', function(e) {
+    e.preventDefault();
+    let description = document.getElementById("new-task-description").value;
+    let taskLi = document.createElement('li');
+    taskLi.innerHTML = `${description} `
+
+    let button = document.createElement("BUTTON")
+    button.innerText = "X"
+    button.addEventListener("click", function(button) {
+      button.target.parentNode.remove();
+    });
+    tasks.appendChild(taskLi);
+    taskLi.appendChild(button);
+    e.target.reset()
+  });
+
+
 });
