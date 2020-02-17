46d0cd46f670e5bea3bf9bfafe5c735b8629705b
Christian Perez <perezchristian1012@gmail.com>
2020-02-17 17:34:22 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+const createTaskForm = document.getElementById("create-task-form");
+// const deleteTasks = document.getElementById("delete-task-form");
+
+createTaskForm.addEventListener("submit", createNewTask);
+// deleteTasks.addEventListener("submit", deleteAllTasks);
 });
+
+const createNewTask = event => {
+event.preventDefault();
+const newTaskDescription = document.getElementById("new-task-description");
+const newTask = document.createElement("li");
+newTask.innerText = newTaskDescription.value;
+console.log(newTask);
+
+appendNewTask(newTask);
+event.target.reset();
+};
+
+const appendNewTask = task => {
+document.getElementById("tasks").appendChild(task);
+};
+
+// const deleteAllTasks = event => {
+// event.preventDefault();
+// const findTasks = document.getElementById("tasks");
+// console.log(findTasks);
+// findTasks.innerText = "";
+// };
