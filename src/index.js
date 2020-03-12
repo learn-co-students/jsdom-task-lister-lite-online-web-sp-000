d065282669be7984224ddcd83fa83f6f6fe0ebf6
BlythePollard <github email address>
2020-03-11 21:40:00 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  
+  const newTaskForm = document.getElementById("create-task-form")
+  const newTaskDescription = document.getElementById("new-task-description")
+  const myToDosList = document.getElementById("tasks")
+
+  newTaskForm.addEventListener("submit", submitDescription) 
+
+  function submitDescription(event) {
+    event.preventDefault();
+    //add li element to ul with content from newTaskDescription
+    const listItem = document.createElement("li")
+    listItem.innerText = newTaskDescription.value
+    myToDosList.appendChild(listItem);
+    return false;
+  }
+
+  //on submit, add text from description form to "my todos" form
 });
