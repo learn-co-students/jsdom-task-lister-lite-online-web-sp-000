380c19df6590c20b86f253885eba69eae41d82b8
Ronald Stephens <rstephens@bluemorpho.cc>
2020-03-17 20:10:58 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+    let form = document.getElementById("create-task-form")
+    let textBox = document.getElementById("new-task-description")
+    let tasks = document.getElementById("tasks") //ul
+    let btn = document.createElement("BUTTON")
+
+    form.addEventListener("submit", function(event){
+        event.preventDefault();
+        console.log("button clicked");
+        let li = document.createElement("li");
+        btn.innerHTML = 'x';
+        li.innerText = `${textBox.value}  `  //form.innerText
+        tasks.append(li)
+        li.append(btn)
+
+        btn.addEventListener("click", function(event) {
+            console.log("button clicked")
+            li.remove()
+        })
+
+    })
+})
