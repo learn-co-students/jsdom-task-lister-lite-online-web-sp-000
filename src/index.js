11a256be5e92270140a4689cd70a0ac13050de79
Educhin <c.s.arinze@gmail.com>
2020-03-18 17:21:18 -0400

    Adds functionality to add and remove task from to-do list

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.getElementById('create-task-form');
+  form.reset()
+  form.addEventListener('submit', addElementToTodoList)
 });
+
+function addElementToTodoList(event) {
+
+  event.preventDefault()
+  
+  addTask()
+
+  let form = document.getElementById('create-task-form');
+  form.reset()
+}
+
+function addTask() {
+  let input = document.getElementById('new-task-description').value;
+  let ul = document.getElementById('tasks');
+  
+  if (input != "") {
+    let li = document.createElement('li')
+    let button = document.createElement('button')
+    button.innerHTML = 'X'
+    button.setAttribute('data-description', input)
+    button.addEventListener('click', removeTask)
+    li.innerHTML = input
+    li.appendChild(button)
+    ul.appendChild(li)
+  } 
+}
+
+function removeTask(e) {
+  e.target.parentElement.remove()
+}
+
+
+
