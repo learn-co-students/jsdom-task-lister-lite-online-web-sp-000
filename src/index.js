27565c8d71d4e60ada814773cd70d5798553d12e
zdsmit <zdsmith001@gmail.com>
2020-03-25 10:01:14 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  
+});
+
+document.addEventListener("submit", (event) => {
+  event.preventDefault()
+  let input = document.getElementById("new-task-description").value
+  let listItem = document.createElement("li")
+  listItem.innerText = input
+  let tasks = document.getElementById("tasks")
+  let add = tasks.appendChild(listItem)
 });
