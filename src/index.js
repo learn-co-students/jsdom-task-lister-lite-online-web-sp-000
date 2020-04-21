fe0ce7142f748f93c0b86974d3265cc7fbf2edd6
Adam Weissman <47704263+AdamWeissman@users.noreply.github.com>
2020-04-21 11:18:19 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+  const newTaskForm = document.getElementById("create-task-form")
+  const newTaskDescription = document.getElementById("new-task-description")
+  const myToDosList = document.getElementById("tasks")
+
+  newTaskForm.addEventListener("submit", submitDescription) 
+
+  function submitDescription(event) {
+    event.preventDefault();
+    const listItem = document.createElement("li")
+    listItem.innerText = newTaskDescription.value
+    myToDosList.appendChild(listItem);
+    return false;
+  }
 });
+
+  
+  
+  
