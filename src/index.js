31a98be41fc5c22b12deee62224f0d55dc1d0913
devludaley <daleyluis@gmail.com>
2020-04-22 03:28:44 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.querySelector("#create-task-form");
+  const newTaskDescription = document.getElementById("new-task-description");
+  // const list = document.getElementById("list");
+  // const taskUl = document.getElementById("task");
+
+  form.addEventListener("submit", function (event) {
+    createNewTask();
+    event.preventDefault();
+    event.target.reset();
+  });
+  function createNewTask() {
+    const taskUl = document.getElementById("task");
+    const task = document.createElement("li");
+    task.innerText = newTaskDescription.value;
+    taskUl.appendChild(task);
+  }
 });
