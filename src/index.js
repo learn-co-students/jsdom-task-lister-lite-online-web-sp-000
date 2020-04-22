20b3f2c349fb88a790ee6e35075d46321904808d
h-miiaah <haim.miiaah5@gmail.com>
2020-04-21 21:24:28 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+  // form and relevant input fields
+  const newTaskForm = document.getElementById("create-task-form");
+
+  // add event listener
+  newTaskForm.addEventListener("submit", createNewTask);
 });
+
+const createNewTask = (event) => {
+  event.preventDefault();
+  // stop from trying to submit
+  const newTaskDescription = document.getElementById("new-task-description");
+  const newTask = document.createElement("li");
+  newTask.innerHTML = newTaskDescription.value;
+
+  appendNewTask(newTask);
+  event.target.reset();
+};
+
+const appendNewTask = (task) => {
+  document.getElementById("tasks").appendChild(task);
+};
