7aa84a6e324fc3737f09a0c9071d59da43e37176
github username <github email address>
2020-04-29 17:24:51 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.querySelector("form");
+  const ulInput = document.getElementById("tasks");
+  form.addEventListener("submit", function(event) {
+    event.preventDefault();
+    console.log(params);
+  });
 });
+
+
+
+// const input = document.getElementById('input');
+// input.addEventListener('click', function(event) {
+//   alert('I was clicked!');
+// });
+
+
+// var form = document.querySelector("form");
+// form.addEventListener("submit", function(event) {
+//   console.log("Saving value", form.elements.value.value);
+//   event.preventDefault();
+// });
+
+// document.querySelector("#id-checkbox").addEventListener("click", function(event) {
+//   document.getElementById("output-box").innerHTML += "Sorry! <code>preventDefault()</code> won't let you check this!<br>";
+//   event.preventDefault();
+// }, false);
