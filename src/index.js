17935954fe58ce0c0ab68d73e57c7a2f0a0ebb02
github username <github email address>
2020-04-29 18:33:46 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -1,27 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const form = document.querySelector("form");
-  const ulInput = document.getElementById("tasks");
-  form.addEventListener("submit", function(event) {
-    event.preventDefault();
-    console.log("event", event.target);
-  });
+  const form = document.getElementById("create-task-form");
+  form.addEventListener("submit", createNewTask);
 });
 
+function createNewTask(event) {
+  event.preventDefault();
+  const ulInput = document.getElementById("tasks");
+  const formInput = document.getElementById("new-task-description");
+  const userValue = formInput.value;
+  const insertElement = document.createElement('li'); 
+  insertElement.innerText = userValue;
+  ulInput.appendChild(insertElement);
+};
 
 
-// const input = document.getElementById('input');
-// input.addEventListener('click', function(event) {
-//   alert('I was clicked!');
-// });
-
-
-// var form = document.querySelector("form");
-// form.addEventListener("submit", function(event) {
-//   console.log("Saving value", form.elements.value.value);
-//   event.preventDefault();
-// });
-
-// document.querySelector("#id-checkbox").addEventListener("click", function(event) {
-//   document.getElementById("output-box").innerHTML += "Sorry! <code>preventDefault()</code> won't let you check this!<br>";
-//   event.preventDefault();
-// }, false);
