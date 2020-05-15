49bcf5570ebcbc9fa919eae72d9352d3fe81b66c
joshuaAjack <Mynameisjoshuajack@gmail.com>
2020-05-14 18:00:07 -0700

    Done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+})
+
+const form = document.getElementById("create-task-form")
+let todos = document.getElementById("tasks")
+const newTaskDescription = document.getElementById("new-task-description")
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault()
+  let element = document.createElement('li')
+  element.innerHTML = newTaskDescription.value
+  todos.appendChild(element)
+  e.target.reset()
+  })
+
