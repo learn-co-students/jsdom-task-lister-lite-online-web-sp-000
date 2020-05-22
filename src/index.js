224e5ee33b58c311a0344969d8889516961bd872
Garrett Thompson <gtcruz.gt@gmail.com>
2020-05-22 00:35:40 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+
+    let form = document.getElementById("create-task-form")
+    console.log("testing",form)
+
+    let textBox =  document.getElementById("new-task-description")
+    // let button = form.querySelectorAll('input')[1]
+
+    let tasks = document.getElementById('tasks') // ul
+
+    form.addEventListener("submit", function(event) {
+      event.preventDefault()
+      console.log("button clicked")
+      let li = document.createElement("li")
+      li.innerText = textBox.value    //form.innerText
+      tasks.append(li)
+    })
+  });
