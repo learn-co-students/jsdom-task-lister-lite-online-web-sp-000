36519ceae917420a9613c71069e0254361620fb7
rehmanali001 <rehmanali001@gmail.com>
2020-05-26 17:03:04 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let el = document.getElementById('create-task-form');
+  el.addEventListener('submit', handleSubmit); 
+  button.addEventListener('click', deleteButton);
+
+  function handleSubmit(e) {
+    let newTask = document.getElementById('new-task-description').value
+    display(newTask);
+    e.preventDefault();
+    e.target.reset()
+
+  }
+
+  function display(testContent) {
+    let content = document.getElementById('tasks');
+    let li = document.createElement('li')
+    li.innerHTML = testContent
+    content.appendChild(li)
+  }
+
 });
