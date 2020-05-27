cace608c368d46747f82424f1d1d4922421d847e
jkellyphilly <joel.kelly.philly@gmail.com>
2020-05-27 17:07:39 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
   const form = document.getElementById('create-task-form');
+  const user_input = document.getElementById('new-task-description');
   let todo_list = document.getElementById('tasks');
 
   form.addEventListener('submit', function(event) {
     let li = document.createElement('li');
-    // TODO: set the text of the new li to what the user entered
+    li.innerHTML = user_input.value;
+    let button = document.createElement('button');
+    button.innerHTML = "X";
+
+    li.appendChild(button);
     todo_list.appendChild(li);
+
+    button.addEventListener('click', function(event) {
+      li.remove();
+    })
+
     event.preventDefault();
   })
 });
