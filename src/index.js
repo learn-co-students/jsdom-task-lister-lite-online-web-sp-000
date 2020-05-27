2d4eec234e8cf4095c0b6fe90b9ca4cf887293da
SLarkworthy <Sarah.Larkworthy@gmail.com>
2020-05-27 19:24:53 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById("create-task-form");
+  const list = document.getElementById("tasks");
+  const tastDescriptionNode = document.getElementById("new-task-description")
+
+  function listSubmit(event) {
+    event.preventDefault();
+    let input = tastDescriptionNode.value;
+    list.appendChild(createList(input));
+    tastDescriptionNode.value = '';
+  }
+
+  function createList(input) {
+    const li = document.createElement('li');
+    li.innerText = input;
+    return li;
+  }
+
+  form.addEventListener('submit', listSubmit);
 });
