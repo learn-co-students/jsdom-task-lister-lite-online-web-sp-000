3f72f85564e7d98bdbe1b15a84db1e805d3a41fa
jkellyphilly <joel.kelly.philly@gmail.com>
2020-05-27 16:44:44 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById('create-task-form');
+  let todo_list = document.getElementById('tasks');
+
+  form.addEventListener('submit', function(event) {
+    let li = document.createElement('li');
+    // TODO: set the text of the new li to what the user entered
+    todo_list.appendChild(li);
+    event.preventDefault();
+  })
 });
