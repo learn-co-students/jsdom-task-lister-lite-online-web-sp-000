9c46255419c8aa618fd3c2c02790f1473a8054bc
doakcovington <doakcovington@protonmail.com>
2020-06-04 10:31:46 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+  //prevents the form from being submitted 
+  document.getElementById("create-task-form").addEventListener("submit", function(event){
+
+  let text = document.getElementById("new-task-description").value;
+
+  //creates LI node
+  let node = document.createElement("LI");
+  //creates text node
+  let textNode = document.createTextNode(text);
+  //appends the text node to the LI node
+  node.appendChild(textNode);
+  //appends the LI node to the list
+  document.getElementById("tasks").appendChild(node);
+
+  event.preventDefault()
+  }, false);
+
 });
