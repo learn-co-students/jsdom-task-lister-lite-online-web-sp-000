b4c28a1a977ee17c22c8de6870b23f7d1487c1ea
Josh Ellis <joshe523@gmail.com>
2020-07-02 11:07:59 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener('DOMContentLoaded', () => {
   // your code here
-});
+  document.querySelector('#create-task').addEventListener(
+    'click',
+    function (e) {
+      const task = document.getElementById('new-task-description').value
+      document.getElementById('new-task-description').value = ''
+      document.getElementById('tasks').innerHTML += `<li>${task}</li>`
+      e.preventDefault()
+    },
+    false
+  )
+})
