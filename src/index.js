7d911cbfaf04e92d67d826fef2d58978cbd1dcf6
Daniel Rooker <dannyrooks@gmail.com>
2020-07-02 14:31:36 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
+
+
+const form = document.getElementById("create-task-form")
+const description = document.getElementById("new-task-description")
+const newTaskPriority = document.getElementById("new-task-priority")
+const taskList = document.getElementById("tasks")
+
+form.addEventListener('submit', handleSubmit)
+
+function handleSubmit(e) {
+  e.preventDefault()
+  let newTask = description.value
+  taskList.innerHTML += `
+    <li>${newTask}</li>
+  `
+
+  description.value = ""
+}
+
+
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
 });
