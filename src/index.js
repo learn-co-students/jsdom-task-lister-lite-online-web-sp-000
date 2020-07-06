da0b4eb6a3000fcbc36f8423d12ae8d727f04739
Roberto-Livi <robertomlivi@outlook.com>
2020-07-05 22:05:00 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  let form = document.querySelector("#create-task-form");
+  let ul = document.querySelector("#tasks");
+  let inputValue = document.getElementById("new-task-description");
+
+  form.addEventListener("submit", function(event){
+    event.preventDefault();
+    let li = document.createElement('li');
+    li.innerText = inputValue.value;
+    ul.appendChild(li);
+
+    let button = document.createElement("button");
+    button.innerHTML = "X";
+    li.appendChild(button);
+
+    button.addEventListener("click", function(){
+        li.remove();
+    });
+
+  })
 });
+
+
