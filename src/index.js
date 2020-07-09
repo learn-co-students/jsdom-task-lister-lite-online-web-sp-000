606f503fabc4509f6e3a179006a860ed5e5bd07f
Firooz Mafakheri <firooz.mafakheri@gmail.com>
2020-07-09 22:45:29 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  
+  let form= document.getElementById('create-task-form')
+  
+  let textBox= document.getElementById('new-task-description')
+  let tasks = document.getElementById('tasks')
+  form.addEventListener('submit', function(event){
+    event.preventDefault()
+   
+    let list= document.createElement('li')
+    list.innerText= textBox.value
+    tasks.append(list)
+  })
+  
 });
