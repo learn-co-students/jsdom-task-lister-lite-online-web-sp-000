3fd643d96fd117e1a48ff71871ea86b141c87c6a
gabejoseph <gabriel.joseph4@gmail.com>
2020-07-10 16:33:22 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.addEventListener("submit", function(event) {
+  event.preventDefault()
+  newTask()
+  });
 });
+
+let todos = document.getElementById("list").textContent
+
+function = newTask(e) {
+  todos = `${e}`
+}
