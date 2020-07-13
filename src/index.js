a90dea573856a0b90d0232d41bc3733997425512
SPerfors <stevecanbuild@gmail.com>
2020-07-13 13:49:18 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  let id = document.querySelector('create-task-form');
+  let taskList = document.querySelector('#tasks');
+  let deleteButton = document.createElement('button');
+  deleteButton.innerText = 'x';
+
+  let placeholder = document.querySelector('#new-task-description');
+
+  id.addEventListener('submit', function(event) {
+    event.preventDefault();
+  }, false);
+
+  document.body.appendChild(element);
+  let ul = document.querySelector('ul.tasks')
+  for (let i = 0; i < 100; i++) {
+    let li = document.createElement('li')
+    li.innerHTML = (i + 1).toString()
+    ul.appendChild(li)
+  } 
+  element.appendChild(ul)
 });
