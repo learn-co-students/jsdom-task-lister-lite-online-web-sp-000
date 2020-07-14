a111b6bf66ea47a172bc38724479240ed94e9f1e
itsbino <charles.sabino@hotmail.com>
2020-07-13 21:23:23 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+    const submitForm = document.getElementById("create-task-form").addEventListener("submit", function(event) {
+        event.preventDefault();
+
+        let listOfTasks = document.getElementById("tasks");
+        let task = document.getElementById("new-task-description").value;
+        let row = document.createElement("li");
+        row.innerText = task;
+        listOfTasks.appendChild(row);
+
+    })
 });
