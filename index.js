bd0d2cd1873a2f0c591d894cefb3fe3e45f3bf63
Anthonyntilelli <Anthony@tilelli.me>
2020-07-17 08:46:36 -0700

    Done.

---
+++ index.js
@@ -0,0 +1,6 @@
+// document.getElementById("new-task-description").addEventListener("submit", function(event){
+//     event.preventDefault()
+//     alert("Was preventDefault() called: " + event.defaultPrevented);
+//   });
+
+  document.getElementById("new-task-description").remove()
