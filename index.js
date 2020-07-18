9d8a562f6772d859bfd7a2be2f4ca3b99da04400
Anthonyntilelli <Anthony@tilelli.me>
2020-07-17 19:21:53 -0700

    Done.

--- index.js
+++
@@ -1,6 +0,0 @@
-// document.getElementById("new-task-description").addEventListener("submit", function(event){
-//     event.preventDefault()
-//     alert("Was preventDefault() called: " + event.defaultPrevented);
-//   });
-
-  document.getElementById("new-task-description").remove()
--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
+  const submitform = document.getElementById("create-task-form")
+  const list = document.getElementById('tasks')
+  submitform.addEventListener("submit", function(event) {
+    let newTask = document.createElement("li");
+    newTask.innerText = submitform.value;
+    list.appendChild(newTask);
+    event.preventDefault();
+}, false);
   // your code here
 });
