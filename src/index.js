6cc12ea960691983551d29dfbedfd9063a80e3d6
Cole Rau <colevrau@gmail.com>
2020-08-10 22:56:40 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+const taskForm = document.querySelector("#create-task-form"); 
+
+const taskList = document.querySelector("#list"); 
+
+taskForm.addEventListener("submit", function(e) {
+  e.preventDefault(); 
+
+  const newTask = document.querySelector("#new-task-description").value; 
+
+  const taskItem = document.createElement("li"); 
+  // debugger;
+
+  taskItem.innerText = newTask;
+
+  // appendChild slaps el on the dom 
+  taskList.appendChild(taskItem); 
 });
