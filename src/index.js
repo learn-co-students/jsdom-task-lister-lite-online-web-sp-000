d7055fbb0cd7d57827335589ff10f4adfc4aabe9
agiletkiewicz <a.letkiewicz@gmail.com>
2020-08-29 20:28:36 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     document.querySelector("#create-task-form").addEventListener("submit", function(event) {
       event.preventDefault();
-      document.getElementById("tasks").innerHTML += document.getElementById("new-task-description").innerHTML;
+      document.getElementById("tasks").innerHTML += document.getElementById("new-task-description").value;
     });
 
 });
