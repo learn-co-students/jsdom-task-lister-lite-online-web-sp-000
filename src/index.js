65929884425d8bd0bc4744432be6393098a59994
ramseychapin <ramsey@ramseychapin.com>
2020-09-06 15:27:29 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  // const taskList = new TaskList();
+
+  const newTaskForm = document.getElementById("create-task-form");
+  const newTaskDescription = document.getElementById("new-task-description");
+  const newTaskPriority = document.getElementById("new-task-priority");
+
+  const taskUl = document.getElementById("tasks");
+
+  newTaskForm.addEventListener("submit", createNewTask)
+
+  });
+
+  // const renderApp = () => (taskUL.innerHTML = taskList.renderTasks());
+
+  const createNewTask = event => {
+  	event.preventDefault();
+
+  	const newTaskDescription = document.getElementById("new-task-description");
+  	const newTask = document.createElement("li");
+  	newTask.innerText = newTaskDescription.value;
+
+  	appendNewTask(newTask);
+  	event.target.reset();
+  };
+
+  const appendNewTask = task => {
+  	document.getElementById("tasks").appendChild(task);
+  };
+
+
