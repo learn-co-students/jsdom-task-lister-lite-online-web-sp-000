fc5caa0e7891291bcddd4cbfbbd98f7d3f7c4228
carrollm2 <carrollm2@gmail.com>
2020-09-15 21:36:20 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  const form = document.getElementById('create-task-form');
+  form.onsubmit = submit;
+
+  function submit(event) {  
+    const newTask = document.getElementById('new-task-description').value;
+    const node = document.createElement("LI");
+    document.getElementById("tasks").appendChild(node).innerHTML = newTask;
+    event.preventDefault();
+  }
+  
 });
