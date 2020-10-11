d087131aef16828158237a434ac09a64839ffc97
bruffin1221 <bruffin1221@gmail.com>
2020-10-11 11:19:43 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+  let form = document.getElementById("create-task-form")
+    form.addEventListener("submit", function(event){event.preventDefault()
+    let name = document.getElementById("new-task-description").value
+    document.querySelectorAll("ul.tasks li").innerHTML = name
+  })})
