3c0ef088462bbf39a8bac0192532b60021661e24
Jivefox <https://github.com/Jivefox>
2020-10-20 11:47:16 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const newTaskDescription = document.getElementById('new-task-description')
+  const createTaskForm = document.getElementById('create-task-form')
+
+  createTaskForm.addEventListener("submit", createNewTask)
 });
+
+
+function createNewTask(event) {
+  event.preventDefault();
+
+  const newTaskDescription = document.getElementById('new-task-description')
+  const newTask = document.createElement("li");
+  newTask.innerText = newTaskDescription.value;
+  // const deleteButton = document.createElement("button")
+  // deleteButton.innerHTML = "X";
+  addTaskToList(newTask);
+  // addDeleteButton(deleteButton);
+  event.target.reset();
+  newTaskDescription.focus();
+};
+
+function addTaskToList(task) {
+  document.getElementById('tasks').appendChild(task);
+};
+
+// function addDeleteButton(button) {
+//   document.querySelector('li').appendChild(button);
+// };
