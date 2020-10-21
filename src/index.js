91dc44fc562e69f8ff8aa5326ab9cb4926cd4c87
chienleow <chienleow1@gmail.com>
2020-10-20 22:49:32 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  // need user input
+  const taskDescription = document.querySelector("#new-task-description")
+  // console.log(taskDescription)
+  // pull the value after the submit button clicked by the user
+  // need the form
+  const taskForm = document.querySelector("#create-task-form")
+  // console.log(taskForm)
+  // listen to when the form is submitted
+  taskForm.addEventListener('submit', function(e) {
+    e.preventDefault() // prevent the default form submit
+    // console.log('test') // always test your code
+    const newTask = taskDescription.value
+    // add to list
+    const list = document.querySelector("#tasks")
+    const newLi = document.createElement('li')
+    newLi.innerText = newTask
+    list.appendChild(newLi)
+  })
 });
