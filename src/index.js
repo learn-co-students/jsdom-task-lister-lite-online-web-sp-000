86508200c644e6afe3bcc2e9f013717667a85fda
Dwayne Weller <mr_dwayne_w@yahoo.com>
2020-10-23 06:49:18 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  function addTaskLi(text) {
+      const taskUl = document.getElementById('tasks');
+      const taskLi = document.createElement('li');
+      taskLi.setAttribute('id', 'task_li');
+      taskLi.innerHTML = text;
+      taskUl.appendChild(taskLi);
+  }
+
+  const newTask = document.getElementById('create-task-form')
+  const taskDesc = document.getElementById('new-task-description')
+  newTask.addEventListener("submit", function(e) {
+      e.preventDefault();
+      console.log(taskDesc.value)
+      addTaskLi(taskDesc.value)
+  });
+
 });
