4c9c63592e3085dc20fe458e0362489582d8654f
mdacht777 <mdacht777@gmail.com>
2020-10-24 18:57:49 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+function createItem(t){
+let node = document.createElement("LI");                 // Create a <li> node
+let textnode = document.createTextNode(t);         // Create a text node
+node.appendChild(textnode);                              // Append the text to <li>
+document.getElementById("tasks").appendChild(node);     // Append <li> to <ul> with id="myList"
+
+}
+
+let f = document.getElementById("create-task-form");
+f.addEventListener("submit", (e) => {
+  e.preventDefault();
+  t = f.elements["new-task-description"]
+  createItem(t.value)
+  t.value="";
+  t.focus();
+});
 });
