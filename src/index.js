0c6ab8bd4c80104b4005aa46b1e3bf6db06c3840
github username <jaycru06@yahoo.com>
2020-10-27 00:49:59 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
+/*
+Deliverables:
+- As a user, I should be able to type a task into the input field.
+- As a user, I should be able to click some form of a submit button.
+- As a user, the task string that I provided should appear on the 
+DOM after the submit button has been activated.
+*/
+
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  handleOnSubmit();
 });
+
+function handleOnSubmit() {
+  const submit = document.querySelector("input[type=submit]");
+  const description = document.getElementById("new-task-description").value;
+  submit.addEventListener("click", function (event) {
+    document.getElementById("tasks").innerHTML += `<li>${description}</li>`;
+    event.preventDefault();
+  }, false);
+};
