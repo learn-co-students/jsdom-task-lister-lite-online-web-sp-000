804ee265d67a2b0c2621a5dd1af50bc079a43d10
CoderJay06 <jaycru06@yahoo.com>
2020-10-29 20:24:28 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -6,41 +6,53 @@ Deliverables:
 DOM after the submit button has been activated.
 */
 
+// const taskForm = document.querySelector("#create-task-form");
+// const taskList = document.querySelector("#tasks");
+
 document.addEventListener("DOMContentLoaded", () => {
     // your code here
     handleOnSubmit();
+    removeTaskOnClick();
     // Show remove button if tasks have been added
-    // if (document.getElementsByClassName("task")) {
-    //     removeTaskOnClick();
-    // }
 });
 
+
+
 function handleOnSubmit() {
-    const submit = document.querySelector("input[type=submit]");
-    const description = document.getElementById("new-task-description");
+    // const submit = document.querySelector("input[type=submit]");
+    const taskList = document.getElementById("tasks");
+    const taskForm = document.querySelector("#create-task-form");
+    const description = document.querySelector("#new-task-description");
     let taskIndex = 0;
-    submit.addEventListener("click", function(event) {
-        document.getElementById("tasks").innerHTML +=
-            `<li class="task">${description.value}</li>`;
-        let removeButton = document.createElement("button");
-        removeButton.className = "remove";
-        removeButton.innerHTML = "X";
-        document.getElementsByClassName("task").item(taskIndex).appendChild(removeButton);
-        removeTaskOnClick(removeButton);
-        taskIndex += 1;
+    taskForm.addEventListener("submit", function(event) {
         event.preventDefault();
+        const taskDescription = document.querySelector("#new-task-description").value;
+        const taskItem = document.createElement("li");
+        taskItem.className = "task";
+        taskItem.innerText = taskDescription;
+        taskList.appendChild(taskItem);
+        // taskList.innerHTML +=
+        //     `<li class="task">${description.value}</li>`;
+        let removeButton = createRemoveTaskButton();
+        // document.getElementsByClassName(taskItem.className).item(taskIndex).innerHTML += removeButton;
+        taskItem.innerHTML += removeButton;
+        // taskIndex += 1;
+        taskForm.reset();
     }, false);
 };
 
-function removeTaskButton() {
-    let removeButton = document.createElement("button");
-    removeButton.className = "remove";
-    removeButton.innerHTML = "X";
+function createRemoveTaskButton() {
+    return '<button class="remove-btn" data-action="delete">X</button>'
 };
 
-function removeTaskOnClick(removeButton) {
-    let task = document.getElementsByClassName("task");
-    removeButton.addEventListener("click", function(event) {
-        removeButton.parentNode.parentNode.removeChild(removeButton.parentNode);
-    });
+function removeTaskOnClick() {
+    const taskList = document.getElementById("tasks");
+    taskList.addEventListener("click", deleteTask);
+};
+
+function deleteTask(event) {
+    const removeTarget = event.target;
+    if (removeTarget.dataset.action == "delete") {
+        removeTarget.parentNode.remove();
+    }
 };
