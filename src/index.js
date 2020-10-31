d242a080a36e5cd21b812158014480973a434710
Jason J Cruz <jaycru06@yahoo.com>
2020-10-31 03:39:32 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,6 @@
-/*
-Deliverables:
-- As a user, I should be able to type a task into the input field.
-- As a user, I should be able to click some form of a submit button.
-- As a user, the task string that I provided should appear on the 
-DOM after the submit button has been activated.
-*/
-
 document.addEventListener("DOMContentLoaded", () => {
-    // your code here
     handleOnSubmit();
     removeTaskOnClick();
-    // Show remove button if tasks have been added
 });
 
 
