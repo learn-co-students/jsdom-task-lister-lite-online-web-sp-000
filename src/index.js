57ab2afd833339bb166a5c4b5412917682bf678a
The-Gentleman <zraja4402@gmail.com>
2020-11-12 11:54:15 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+document.addEventListener("DOMContentLoaded", function(e) {
+  let submit = document.getElementById('create-task-form');
+
+  submit.addEventListener("submit", function(e) {
+    e.preventDefault();
+    let inputValue = e.target.querySelector("#new-task-description").value;
+    let list = document.getElementById("list");
+    let ul = document.createElement('ul');
+    list.appendChild(ul);
+    let li = document.createElement('li');
+    ul.appendChild(li);
+    li.innerHTML = inputValue;
+  })
+})
+
+
+
