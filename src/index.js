a0eea706bea9b7f5836158e1f0e28f0fdf9ea5e1
Scott Hoisington <hoyzington@gmail.com>
2020-11-28 07:13:05 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const form = document.getElementById('create-task-form')
-  form.addEventListener('submit', (e) => addTask(e))
+  const form = document.getElementById('create-task-form');
+  form.addEventListener('submit', (e) => addTask(e));
 });
 
 function addTask(e) {
-  e.preventDefault()
-  const task = document.getElementById('new-task-description')
-  const ul = document.getElementById('tasks')
-  const li = document.createElement('li')
-  li.innerText = task.value
-  ul.appendChild(li)
-  e.target.reset()
+  e.preventDefault();
+  const task = document.getElementById('new-task-description');
+  const ul = document.getElementById('tasks');
+  const li = document.createElement('li');
+  li.innerText = task.value;
+  ul.appendChild(li);
+  e.target.reset();
 }
