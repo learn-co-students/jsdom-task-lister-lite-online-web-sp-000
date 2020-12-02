0da1275313c700d7e47c938d2b1fbe4fc26ebf4c
mlgvla <mlgerardvla@gmail.com>
2020-12-01 20:33:14 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+    // your code here
+
+    const taskForm = document.getElementById("create-task-form");
+ 
+    taskForm.addEventListener("submit", function(e) {
+        e.preventDefault();
+        const taskText = document.getElementById("new-task-description");
+        const taskUl = document.getElementById("tasks");
+        const taskLi = document.createElement("li");
+        taskLi.innerText = taskText.value;
+        taskUl.appendChild(taskLi);
+        e.target.reset()
+    })
 });
