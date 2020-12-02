69940ec0e506efcbc78158337e6b42383c0525c4
Jasmine Carter <jasmine.carter87@gmail.com>
2020-12-02 01:24:14 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+const taskUl = document.getElementById("tasks")
+let li = document.createElement("li")
+
+document.querySelector("input[type=submit]").addEventListener("click", function(event){
+  let input = document.getElementById("new-task-description").value
+  li.textContent = input
+  taskUl.appendChild(li);
+  event.preventDefault()
 });
+});
+  // your code here
