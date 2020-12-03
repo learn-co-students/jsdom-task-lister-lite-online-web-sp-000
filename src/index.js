c721c54d702299d436b2a1622fed9c20355d23fb
Gearz <shanegeary@yahoo.com>
2020-12-03 15:18:51 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let createTask = document.getElementById('create-task-form') 
+  createTask.addEventListener('submit', submitTask)
+
+  // const delButton = document.createElement('BUTTON');
+  // delButton.textContent = "X"; 
+  // listNode.appendChild(delButton); 
+
+function submitTask(t){
+  let newTask = document.getElementById('new-task-description').value
+  display(newTask);
+  t.preventDefault();
+  t.target.reset()
+  
+  
+}
+
+function display(task){
+  let taskList = document.getElementById('tasks');
+  let list = document.createElement('li');
+  list.innerHTML = task
+  taskList.appendChild(list)
+  }
 });
+
+
