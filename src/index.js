a17f7d6450a220c36241bafbcf66e8e14108566d
ArslanInfinite <ali.arslan319@gmail.com>
2020-12-20 20:14:09 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+
+  const userInput = document.getElementById('new-task-description');
+  const theForm = document.getElementById('create-task-form');
+  const taskList = document.getElementById('tasks');
+
+  theForm.addEventListener('submit', function(e) {
+    e.preventDefault();
+    let listItem = document.createElement('li');
+    listItem.innerHTML = userInput.value
+    taskList.appendChild(listItem);
+  }); 
+
+})
