88ce068409a9fc7d8897063752eb86a3dc7ec27e
keelsk <keelsk@gmail.com>
2020-12-25 22:53:15 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.getElementById('create-task-form');
+  // let input = document.getElementById('new-task-description');
+
+  form.addEventListener('submit', function(e){
+    e.preventDefault();
+
+    let taskContent = document.getElementById('new-task-description').value;
+
+    let newTask = document.createElement('li');
+  
+    newTask.innerText = taskContent;
+  
+    document.getElementById('tasks').appendChild(newTask);
+
+
+  });
+
+  // input.addEventListener('keydown', function(event){
+  //   taskContent += event.key; 
+  // });
+
+
+
+
+
 });
+
