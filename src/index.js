da4d5696e4c38bf79573520e2be238105fa77c56
Brian Vehige <brianvehige@gmail.com>
2021-01-13 23:17:24 -0800

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let createTask = document.getElementById('create-task-form')
+  let submit = document.querySelector("input[type=submit]")
+
+  createTask.addEventListener('submit', function(event) {
+    addTask();
+    event.preventDefault();
+    resetInput();
+  });
+
 });
+
+  function addTask() {
+    let newTask = document.getElementById('new-task-description').value
+    let list = document.getElementById('tasks')
+    let line = document.createElement('li')
+
+    let addLine = list.appendChild(line)
+    addLine.innerHTML = newTask;
+  } 
+
+  function resetInput(){
+    document.getElementById('new-task-description').value = ""
+  }
