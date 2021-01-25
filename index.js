a473223d38dd144dbac66fd650b4132a19882083
JWDonovan <jameswilliamdonovan@gmail.com>
2021-01-25 15:52:18 -0500

    Done.

---
+++ index.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const newTaskForm = document.getElementById('create-task-form');
+  const newTaskDescription = document.getElementById('new-task-description');
+  const newTaskPriority = document.getElementById('new-task-priority');
+  const newTaskUl = document.getElementById('tasks');
+
+  newTaskForm.addEventListener('submit', createNewTask);
+});
+
+const createNewTask = function(e) {
+  e.preventDefault();
+
+  const newTaskDescription = document.getElementById('new-task-description');
+  
+  const newTask = document.createElement('li');
+  newTask.innerText = newTaskDescription.value;
+  appendNewTask(newTask);
+
+  e.target.reset();
+}
+
+const appendNewTask = function(task) {
+  document.getElementById('tasks').appendChild(task);
+};
