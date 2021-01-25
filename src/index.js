9604f0b43cc24b4349e903a5b7a5e94a9f7cca0e
JWDonovan <jameswilliamdonovan@gmail.com>
2021-01-25 15:53:15 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener('DOMContentLoaded', function() {
   // your code here
+  const newTaskForm = document.getElementById('create-task-form');
+  const newTaskDescription = document.getElementById('new-task-description');
+  const newTaskPriority = document.getElementById('new-task-priority');
+  const newTaskUl = document.getElementById('tasks');
+
+  newTaskForm.addEventListener('submit', createNewTask);
 });
+
+const createNewTask = function(e) {
+  e.preventDefault();
+
+  const newTaskDescription = document.getElementById('new-task-description');
+  
+  const newTask = document.createElement('li');
+  newTask.innerText = newTaskDescription.value;
+  appendNewTask(newTask);
+
+  e.target.reset();
+}
+
+const appendNewTask = function(task) {
+  document.getElementById('tasks').appendChild(task);
+};
