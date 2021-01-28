f930ffb50b7bc39b19336cc82027db1744aa5462
AmyFong82 <amyhfong@gmail.com>
2021-01-27 23:30:09 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.querySelector("input[type=submit]").addEventListener("click", function(event) {
+  	let newList = document.getElementById("new-task-description").value;
+  	document.getElementById("tasks").innerHTML += "<li>" + newList + "</li>";
+    event.preventDefault();
+
+  });
 });
