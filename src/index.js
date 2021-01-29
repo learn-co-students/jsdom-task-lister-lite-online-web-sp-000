9c72dfc8d864b7dd19391cd552a8ac0af0f73e0e
anpham148 <anpham148@gmail.com>
2021-01-29 13:15:39 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const inputBox = document.getElementById('new-task-description');
+  const submit = document.querySelector('input[type="submit"]');
+  let toDoList = [];
+  let count = 0;
+  
+submit.addEventListener('click', (e) => {
+  e.preventDefault();
+  let input = inputBox.value;
+  toDoList.push(input);
+  let ul = document.querySelector('ul#tasks');
+
+  let li = document.createElement('li');
+  li.id = count;
+  count++;
+  li.innerHTML = toDoList[toDoList.length - 1];
+  ul.appendChild(li);
+
+  let btn = document.createElement('button');
+  btn.innerHTML = 'X';
+  li.appendChild(btn);
+
+  btn.onclick = function () {
+    document.getElementById(li.id).remove();
+   };
+   inputBox.value = "";
 });
+  });
