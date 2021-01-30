880026ce04609539a82da33a1cf77e31c47ebb70
jrog20 <jaimerogstad@gmail.com>
2021-01-30 16:39:50 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
   document.getElementById("#create-task-form").addEventListener("submit", function(event) {
     event.preventDefault();
+    addListItem();
   });
+
+  let newTaskDescription = document.getElementById("new-task-description");
+  let tasks = document.getElementById("tasks");
+
+  function addListItem(){
+    let newTask = document.createElement("li");
+    newTask.textContent = newTaskDescription.value;
+    tasks.appendChild(newTask);
+  }
 });
