fa28e9bec76d49fb47607fd512a8e94f80f0b835
khoinguyenkc <khoinguyenkc@gmail.com>
2021-02-03 00:37:42 -0800

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+
+    //append child to id tasks, the innerHTML should be copied from id="new-task-description" 
+    function updateTodoList(content) {
+      let liTaskItem = document.createElement("li")
+      liTaskItem.innerHTML = content; 
+      let list = document.querySelector("div#list ul")
+      list.appendChild(liTaskItem)
+    };
+
+
+
+
+
+
+
+
+  //event listener for form submit or click submit button
+  let form = document.getElementById("create-task-form")
+
+  function handleFormSubmit() {
+    event.preventDefault();
+    let newTask = document.getElementById("new-task-description");
+    updateTodoList(newTask.value);
+  } 
+
+  form.addEventListener('submit', handleFormSubmit )
+
+
+
+
 });
