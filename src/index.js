a460e2a9f48874a33567edaa83873a8ec5d2cc4c
Catherine Gloria <catherinegloria@gmail.com>
2021-02-02 17:39:30 -0800

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  const form = document.getElementById("create-task-form");
+  const description = document.getElementById("new-task-description").value;
+  const text = document.createTextNode(description);
+  const ul = document.getElementById("tasks");
+  const newLi = document.createElement("li");
+
+  function createTask(){
+    form.addEventListener("submit", function(e){
+      e.preventDefault(); //stop submit
+
+      let li = newLi.appendChild(text); //<li>description</li>
+      ul.appendChild(li); //<ul><li>...</li></ul>
+    };
+  };
+
+  createTask()
+
 });
