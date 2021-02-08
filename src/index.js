06e8cfc995bd04f6fa089dd43915617c942d2de1
john million <jpmillion@comcast.net>
2021-02-08 17:40:43 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.addEventListener('submit', function(e) {
+    let task = document.getElementById('new-task-description').value;
+    addTask(task);
+    e.preventDefault();
+  });
 });
+
+let ul = document.getElementById('tasks');
+
+
+
+function addTask(task) {
+  let li = document.createElement('li');
+  li.innerText = task
+  ul.appendChild(li);
+}
+
+
+
+
