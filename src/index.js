db3f48383bbb1fd829ea41c4e2d9aa84fb2c48ed
TrueC-D <aletheacary@gmail.com>
2021-02-20 08:08:13 -0800

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", function(event) {
   // your code here
-});
+
+  const createNewTask = function(event) {
+    event.preventDefault();
+    const newTask = document.createElement('li');
+    newTask.innerText = document.getElementById('new-task-description').value;
+    document.getElementById("tasks").appendChild(newTask);
+  };
+
+  document.getElementById("create-task-form").addEventListener('submit', createNewTask);  
+
+  })
