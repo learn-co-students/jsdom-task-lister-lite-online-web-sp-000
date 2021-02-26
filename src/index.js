b24010e4b66eee7672130d13f00ad3d60aabb9ff
nllew <n.lewis.perc@gmail.com>
2021-02-25 19:18:25 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    newTask(input.value);
+  });
 });
+
+const form = document.getElementById('create-task-form');
+const list = document.getElementById('tasks');
+const input = document.getElementById('new-task-description')
+
+function newTask(task) {
+  let li = document.createElement('li');
+  let button = document.createElement('button');
+  li.innerText = task;
+  list.appendChild(li);
+  input.value = "";
+  list.lastChild.appendChild(button);
+  button.innerText = "X";
+}
