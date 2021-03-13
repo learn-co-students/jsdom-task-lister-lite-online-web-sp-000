fc7135d4e93dfb6ce9e9ff790b9a2a196b8c1005
Carrie Mathieu <carrieburks@gmail.com>
2021-03-12 19:17:23 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  
+  const form = document.getElementById('create-task-form')
+
+  form.addEventListener('submit', (event) =>{
+    event.preventDefault(); 
+    const text = document.getElementById('new-task-description');
+    const lists = document.getElementById('tasks');
+
+    todos = document.createElement("li");
+    todos.innerText = text.value;
+    lists.appendChild(todos);
+    event.preventDefault(); 
+  })
+  
 });
