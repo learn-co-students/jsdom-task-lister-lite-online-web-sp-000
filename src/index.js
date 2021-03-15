660b0db4df668a1fc330b7242fde1552db23f991
Jenise Ruehle <jeniseruehle@gmail.com>
2021-03-15 11:35:16 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
   let taskForm = document.querySelector("create-task-form")
   let newTask =  document.querySelector("new-task-description")
+  
   taskForm.addEventListener("submit", (event) => {
     event.preventDefault()
     console.log("form submit", newTask.value)
