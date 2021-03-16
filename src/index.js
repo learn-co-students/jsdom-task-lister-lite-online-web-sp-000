6935e39bd49c18979087252051f80491fc865106
romanolivi <romano_livi@outlook.com>
2021-03-16 19:58:40 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
+const form = document.querySelector("#create-task-form");
+const list = document.querySelector("#tasks");
+
+form.addEventListener("submit", function(event) {
+  event.preventDefault();
+  const newTask = document.querySelector("#new-task-description").value;
+
+  list.innerHTML += `<li> ${newTask} 
+  <button data-action = "delete"> X </button>
+  </li>`;
+});
+
+list.addEventListener("click", function (e) {
+  if (e.target.dataset.action === "delete") {
+    e.target.parentElement.remove();
+  }
+});
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  
 });
