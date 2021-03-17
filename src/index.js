f64a2e3631868b57433b9fa246ce1e729a3186d7
RSigmaSG <sg.comparch@gmail.com>
2021-03-17 00:46:55 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.querySelector('input[value="Create New Task"]').addEventListener("click", function(e){
+    e.preventDefault();
+    createTask();
+  });
+
+  function createTask() {
+    let text = document.getElementById("new-task-description").value;
+    let list = document.getElementById('tasks');
+    let li = document.createElement('li');
+    li.innerHTML = `${text}<button data-description="${text}">X</button>`;
+    list.appendChild(li);
+    document.getElementById("new-task-description").value = "";
+    document.querySelectorAll('#tasks li button').forEach(button => button.addEventListener("click", function(e){
+      e.preventDefault();
+      button.parentNode.remove()
+      console.log(button)
+    }));
+  }
+
+  document.querySelectorAll('#tasks li button').forEach(button => button.addEventListener("click", function(e){
+    e.preventDefault();
+    button.parentNode.remove()
+  }));
+
 });
