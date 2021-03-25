d463332a579c5acb84bfe0653eccbd4d150462af
djenkala <daniel@jenkala.com>
2021-03-24 21:10:35 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  document.querySelector("#create-task-form").addEventListener("submit", function(event) {
+    event.preventDefault();
+  }, false);
+
 });
