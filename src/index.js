2a02a46506063b35d0830b97f8cdd79118f904d7
sbigelo <sean.bigelow1@gmail.com>
2021-04-06 16:17:29 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let element = document.getElementById("new-task-description");
+  let submit = document.getElementById("create-task-form");
+  let task = document.getElementById("tasks");
+  
+
+  function generate(submit) {
+    submit.preventDefault();
+  const li = document.createElement('li');
+    li.innerHTML = element.value;
+    task.appendChild(li);
+  }
+  submit.addEventListener("submit", generate)
+
 });
