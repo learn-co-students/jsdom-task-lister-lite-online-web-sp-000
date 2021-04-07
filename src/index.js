b58d75d084f3a98458e34691f44e299776ee4e9d
Lexi Sandoval <lexi.sandoval9@gmail.com>
2021-04-06 19:14:58 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
+const form = document.getElementById("create-task-form");
+const input = document.getElementById("new-task-description");
+const tasks = document.getElementById("tasks");
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  
+  form.addEventListener("submit", function(event) {
+
+    event.preventDefault(); 
+  
+    const li = document.createElement("li");
+    li.innerText = input.value;
+    tasks.appendChild(li);
+    input.value = "";
+  });
+
 });
