27886b45af70961933d90cf8e599f36c7ab853f1
Alison Williams <apw5011@gmail.com>
2021-04-14 14:19:46 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const taskForm = document.querySelector("#create-task-form");
+
+  const taskList = document.querySelector("#tasks");
+
+  taskForm.addEventListener("submit", function(event) {
+    event.preventDefault();
+    const newTask = document.querySelector("#new-task-description").value;
+
+    taskList.innerHTML += `<li> ${newTask} </li>`;
+    taskForm.reset();
+  });
 });
