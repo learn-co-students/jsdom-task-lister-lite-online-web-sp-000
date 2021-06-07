e5b8381834934967d39afecabba7fe1eae7ceb79
Luis Martinez <luis_mmartinez@live.com>
2021-06-06 20:50:36 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
   let submit = document.getElementById("create-task-form").querySelectorAll('input')[1];
-  submit.addEventListener("click", function(event) {
-    event.preventDefault();
+  submit.addEventListener.call(this, "click", function(event) {
+    this.preventDefault();
     let input = document.getElementById("create-task-form").querySelectorAll('input')[0];
     let ul = document.getElementById('tasks');
     let li = document.createElement('li');
@@ -11,8 +11,12 @@ document.addEventListener("DOMContentLoaded", () => {
     let button = document.createElement('button');
     button.innerHTML = "X";
     li.appendChild(button);
-    button.addEventListener("click", function(event) {
+    button.addEventListener("click", function() {
+      if (li.innerText === "X") {
+        Array.from(ul.querySelectorAll('li')).filter( y => y.innerText === "X").forEach( x => x.remove())
+      } else {
       li.remove();
+      }
     });
     input.value = null;
   }); 
