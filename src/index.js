dccfc698d3b5c7cbbd0b57d9cf553de0a1de22ea
Technicallyaustin <Technicallyaustin@gmail.com>
2021-06-16 17:13:04 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  // selects the form id from the document 
+  const form = document.querySelector('form');
+
+  // allows the form to be submitable
+  form.addEventListener('submit', function(event){
+    event.preventDefault();
+
+    // accepts user input and returns the value
+    let input = document.getElementById('new-task-description');
+    let inputValue = input.value;
+
+    // Creates a li and assigns the inner text value as the user input
+    let li = document.createElement('li');
+    li.innerText = inputValue;
+
+    // assigns tasklist as the ul for tasks and appends the newly create li to the tasklist
+    let taskList = document.querySelector('ul#tasks');
+    taskList.append(li);
+    inputValue = '';
+
+  })
 });
