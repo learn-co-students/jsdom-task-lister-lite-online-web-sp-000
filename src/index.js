7791ae737d85b58328cd1235bc7ea41b61e4c2c2
Lj <tnedutsl457892@gmail.com>
2021-06-30 22:27:41 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,66 @@
+//taskList
+class TaskList {
+  constructor() {
+    this.tasks = [];
+  }
+
+  createNewTask(description) {
+    const newTask = new Task(description);
+    this.tasks.push(newTask);
+  }
+
+  renderTasks() {
+    return this.tasks.map((task) => task.render()).join("");
+  }
+
+  deleteTask(description) {
+    this.tasks = this.tasks.filter((task) => task.description !== description);
+  }
+}
+
+ class Task {
+  constructor(description) {
+    this.description = description;
+  }
+
+  render() {
+    return `
+      <li>
+        ${this.description}
+        <button data-description="${this.description}">X</button>
+      </li>
+      `;
+  }
+}
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  //initialize taskList class
+  const taskList = new TaskList();
+  // grab DOM elements
+  // form and input fields
+  const newTaskForm = document.getElementById("create-task-form");
+  const newTaskDescription = document.getElementById("new-task-description");
+  // const newTaskPriority = document.getElementById("new-task-priority");
+  
+  // ul where tasks will live on DOM
+  const taskUl = document.getElementById("tasks");
+
+  const renderApp = () => (taskUl.innerHTML = taskList.renderTasks()); 
+  //attach event listeners 
+  newTaskForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    //task string to appear on DOM after submit button activated
+    taskList.createNewTask(newTaskDescription.value);
+    // reset form
+    e.target.reset();
+    renderApp();
+  });
+
+  // delete event
+  taskUl.addEventListener("click", (e) => {
+    if (e.target.nodeName === "BUTTON") {
+      taskList.deleteTask(e.target.dataset.description);
+      renderApp();
+    }
+  });
 });
